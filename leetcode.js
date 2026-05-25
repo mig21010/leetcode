ee6ea5f6c016908e74bfa3d78360ed26808f4d7c
mig21010 <mig2190@hotmail.com>
2026-05-25 00:11:11 -0600

    🔨 refactor(leetcode.js): remove unnecessary comments and improve code readability 📝 docs(leetcode.js): add missing descriptions for maxProfit and twoSum functions

--- leetcode.js
+++ leetcode.js
@@ -2,6 +2,12 @@
 
 //brute force O(n2) Time to Buy and Sell Stock
 
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.*/
+
 function maxProfit(prices){
 
 	let maxP = 0 
@@ -21,6 +27,8 @@ function maxProfit(prices){
 }
 
 //single pass O(n)
+//Rastrea el mínimo precio visto y la máxima ganancia:
+
 
 
 function maxProfit(prices){
@@ -42,6 +50,12 @@ function maxProfit(prices){
 
 
 //Two sum given array of intgers nums and target retrurn the indices of two numbers
+
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.*/
 
 function twoSum(nums, target){
 
@@ -65,6 +79,7 @@ function twoSum(nums, target){
 
 
 //Contains Duplicate
+/*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.*/
 function containsDuplicate(nums){
 
 	const seen = new Set()
